ba5b613987fd685bbfeb35eb6c61c9fbf052990e
Ibragim <57891324+eastana@users.noreply.github.com>
2020-08-27 09:33:04 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,40 +1,38 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 #define ull unsigned long long
 #define ll long long
-#define vint vector<int>
-#define vstr vector<string>
-#define vdbl <double>
+#define vint vector < int >
+#define vstr vector < string >
+#define vdbl < double >
 #define pb push_back
 
 int partition(vector < int > & a, int start, int end) {
-      int pivot = a[end];
-      int P_index = start;
-      for (int i = start; i < end; i++) {
-        if (a[i] <= pivot) {
-          swap(a[i], a[P_index]);
-          P_index++;
-        }
-      }
-      swap(a[end], a[P_index]);
-      return P_index;
-}
-    
-    
-void quicksort(vector < int > & a, int start, int end) {
-    if (start < end) {
-      int P_index = partition(a, start, end);
-      quicksort(a, start, P_index - 1);
-      quicksort(a, P_index + 1, end);
+  int pivot = a[end];
+  int P_index = start;
+  for (int i = start; i < end; i++) {
+    if (a[i] <= pivot) {
+      swap(a[i], a[P_index]);
+      P_index++;
     }
+  }
+  swap(a[end], a[P_index]);
+  return P_index;
+}
+
+void quicksort(vector < int > & a, int start, int end) {
+  if (start < end) {
+    int P_index = partition(a, start, end);
+    quicksort(a, start, P_index - 1);
+    quicksort(a, P_index + 1, end);
+  }
 }
 
 void merge(vector < int > & a, int left, int right, int mid) {
   int n1 = mid - left + 1;
   int n2 = right - mid;
-  int l[mid+right], r[mid+right];
+  int l[mid + right], r[mid + right];
   for (int i = 0; i < n1; i++)
     l[i] = a[left + i];
   for (int j = 0; j < n2; j++)
@@ -72,25 +70,28 @@ void mergesort(vector < int > & a, int left, int right) {
   }
 }
 
-bool binsearch(vector<int> &a, int left, int right, int f){
-  while(left<=right){
-      int mid=(left+right)/2;
-      if(a[mid]==f){
-          return true;
-      } else if(f>a[mid]){
-          left=mid+1;
-      } else{
-          right=mid-1;
-      }
+bool binsearch(vector < int > & a, int left, int right, int f) {
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    if (a[mid] == f) {
+      return true;
+    } else if (f > a[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
-    return false;
+  return false;
 }
 
-
 int main() {
-    
+  int n;
+  cin >> n;
+  vint arr;
+  for (int i = 0, k; i < n; i++) {
+    cin >> k;
+    arr.pb(k);
+  }
 
-
-
-    return 0;
+  return 0;
 }
